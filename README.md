25da7fe8b7af048c05eeb3ef4cdd8a86c42111de
xu42 <xu42.cn@gmail.com>
2018-09-07 22:24:51 +0800

    0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # coremail-address-book
+
+[![Build Status](https://travis-ci.org/dpu/coremail-address-book.svg)](https://travis-ci.org/dpu/coremail-address-book)
+[![release](https://img.shields.io/github/release/dpu/coremail-address-book.svg)](https://github.com/dpu/coremail-address-book/releases)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/dpu/coremail-address-book/master/LICENSE)
+[![platform](https://img.shields.io/badge/platform-%20windows%20%7C%20linux%20%7C%20freebsd%20%7C%20darwin%20-yellow.svg)](https://github.com/dpu/coremail-address-book/releases)
+
 📧Coremail邮件系统组织通讯录一键导出
+
+## 安装
+
+### 二进制包
+在 [releases](https://github.com/dpu/coremail-address-book/releases) 有各个系统的zip包, 根据系统下载对应的zip文件并解压后使用。
+
+### 源码编译
+```shell
+$ git clone git@github.com:dpu/coremail-address-book.git
+$ cd coremail-address-book
+$ go build main.go
+$ ./main -h
+```
+
+### 使用
+```shell
+// 查看帮助信息
+$ main -h
+Usage of ./main:
+  -coremail_cookie string
+        Coremail value in Request Cookie
+  -coremail_sid string
+        Coremail.sid value in Request Cookie
+  -host string
+        Coremail host (default "mail.dlpu.edu.cn")
+
+// 执行导出
+// -coremail_cookie 与 -coremail_sid 可在浏览器登录邮箱查看
+$ ./main -coremail_cookie=YOUR_COOKIE_COREMAIL -coremail_sid=YOUR_COOKIE_COREMAIL_SID
+```
+
+![](./cookies.png)
---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"encoding/csv"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var (
+	host              string
+	cookieCoreMail    string
+	cookieCoreMailSid string
+)
+
+func init() {
+	flag.StringVar(&host, "host", "mail.dlpu.edu.cn", "Coremail host")
+	flag.StringVar(&cookieCoreMail, "coremail_cookie", "", "Coremail value in Request Cookie")
+	flag.StringVar(&cookieCoreMailSid, "coremail_sid", "", "Coremail.sid value in Request Cookie")
+}
+
+func main() {
+	flag.Parse()
+	url := buildUrl(host, cookieCoreMailSid)
+
+	respCount := getResp(url, 0, 1, fmt.Sprintf("Coremail=%s", cookieCoreMail))
+
+	var responseCount Response
+	json.Unmarshal([]byte(respCount), &responseCount)
+
+	f, err := os.Create(host + ".email_list.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	f.WriteString("\xEF\xBB\xBF")
+	w := csv.NewWriter(f)
+	w.Write([]string{"name", "email"})
+
+	for i := 0; i < responseCount.Total; i += 100 {
+		respList := getResp(url, i, 100, fmt.Sprintf("Coremail=%s", cookieCoreMail))
+		var responseList Response;
+		json.Unmarshal([]byte(respList), &responseList)
+		for _, item := range responseList.Items {
+			w.Write([]string{item.Name, item.Email})
+		}
+	}
+	w.Flush()
+}
+
+func buildUrl(host string, sid string) string {
+	return "http://" + host + "/coremail/s/json?func=oab%3AlistEx&sid=" + sid;
+}
+
+func buildPostData(start int, limit int) string {
+	return fmt.Sprintf("{\"dn\":\"a\",\"returnAttrs\":[\"true_name\",\"email\"],\"start\":%d,\"limit\":%d,\"defaultReturnMeetingRoom\":false}", start, limit)
+}
+
+func getResp(url string, start int, limit int, cookie string) string {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("POST", url, strings.NewReader(buildPostData(start, limit)))
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Cookie", cookie)
+	resp, err := client.Do(req)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+
+	return string(body)
+}
+
+type Response struct {
+	Code  string `json:"code"`
+	Total int    `json:"total"`
+	Items []Item `json:"var"`
+}
+
+type Item struct {
+	Name  string `json:"true_name"`
+	Email string `json:"email"`
+}
